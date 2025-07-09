03e857f2ed79cf821a9e67e2dadec112da0dec85
Morozov Mikhail <memphis.sail@gmail.com>
2025-07-09 15:24:10 +1000

    Add unplugin vue router, fix tailwind, update dependencies

---
+++ src/pages/(home).vue
@@ -0,0 +1,7 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+    <h1 class="text-3xl text-center">Main page</h1>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
+import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter(),
     vue(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {

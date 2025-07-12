58cb7c71f3da0094a2332daa3b7e271a393cc386
Morozov Mikhail <memphis.sail@gmail.com>
2025-07-12 14:01:24 +1000

    add @iconify/vue, refactor Input component dor using Icon component inside

--- src/components/elements/Input.vue
+++ src/components/elements/Input.vue
@@ -1,55 +1,62 @@
 <script setup lang="ts">
-import { Component } from "vue";
+import { useId } from 'vue'
+import { Icon } from '@iconify/vue'
 
 type inputTypes =
-	| "text"
-	| "password"
-	| "email"
-	| "number"
-	| "color"
-	| "date"
-	| "datetime-local"
-	| "month"
-	| "tel"
-	| "time"
-	| "url"
-	| "week";
+  | 'text'
+  | 'password'
+  | 'email'
+  | 'number'
+  | 'color'
+  | 'date'
+  | 'datetime-local'
+  | 'month'
+  | 'tel'
+  | 'time'
+  | 'url'
+  | 'week'
 
 const props = defineProps<{
-	type: inputTypes;
-	placeholder?: string;
-	disabled?: boolean;
-	label?: string;
-	icon?: Component;
-}>();
-const value = defineModel("value");
-const message = defineModel("message");
-const inputId = Math.random().toString(36).substring(2);
+  type: inputTypes
+  placeholder?: string
+  disabled?: boolean
+  label?: string
+  icon?: string
+  iconDimension?: string
+  iconColor?: string
+}>()
+
+const value = defineModel('value')
+const message = defineModel('message')
+const inputId = useId()
 </script>
 
 <template>
-	<div class="input">
-		<label :for="inputId" v-if="props.label" class="input-label">
-			{{ props.label }}
-		</label>
-		<div class="relative flex items-center">
-			<input
-				:type="props.type"
-				v-model="value"
-				:id="inputId"
-				:placeholder="props.placeholder"
-				:disabled="props.disabled"
-				class="input-field"
-				:class="{
-					'pr-[40px]': props.icon,
-				}"
-			/>
-			<span v-show="props.icon" class="top-1/2 right-[16px] absolute -translate-y-1/2">
-				<props.icon />
-			</span>
-		</div>
-		<span v-show="message" class="input-message">
-			{{ message }}
-		</span>
-	</div>
+  <div class="input">
+    <label :for="inputId" v-if="props.label" class="input-label">
+      {{ props.label }}
+    </label>
+    <div class="relative flex items-center">
+      <input
+        :type="props.type"
+        v-model="value"
+        :id="inputId"
+        :placeholder="props.placeholder"
+        :disabled="props.disabled"
+        class="input-field"
+        :class="{
+          'pr-[40px]': props.icon,
+        }"
+      />
+      <span v-show="props.icon" class="top-1/2 right-[16px] absolute -translate-y-1/2">
+        <Icon
+          :icon="props.icon"
+          :style="{ fontSize: props.iconDimension, color: props.iconColor }"
+        />
+      </span>
+    </div>
+    <span v-show="message" class="input-message">
+      {{ message }}
+    </span>
+  </div>
 </template>
